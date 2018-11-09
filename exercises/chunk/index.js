a3c730326781a28a49086f3f151783e1f60aafe7
Turutupa <alberto.delgado@sap.com>
2018-11-09 16:50:58 +0100

    solved

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // MI SOLUCIÓN
+    /*
+    let subArr = [];
+    let finalArr = [];
+    array.map((element, i) => {
+        subArr.push(element)
+        if (array.length % size !== 0 && i + 1 === array.length) {
+            finalArr.push(subArr)
+        }
+        if (subArr.length >= size) {
+            finalArr.push(subArr);
+            subArr = [];
+        }
+
+    })
+    return finalArr
+    */
+
+    // PRIMERA SOLUCIÓN DE STEPHEN
+    /*
+    const chunked = [];
+
+    for(let element of array){
+        const last = chunked[chunked.length-1];
+        
+        if(!last || last.length === size){
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+    
+    return chunked
+    */
+
+    // SEGUNDA SOLUCIÓN DE STEPHEN
+    const chunked = [];
+    let index = 0;
+
+    while(index< array.length){
+        chunked.push(array.slice(index, index + size))
+        index += size
+    }
+    return chunked;
+}
 
 module.exports = chunk;
