3f647895f5ef305d0731c42b9995a7d3c8611ff1
Turutupa <alberto.delgado@sap.com>
2018-11-20 15:53:55 +0100

    solved

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -14,9 +14,50 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
-function matrix(n) {
-    let arr = [];
-    let subArr = [];
+
+function matrix(n, arr, subArr, direction, colDeduct, rowDeduct, count) {
+    if (count === (n * n)) {
+        return arr;
+    } else {
+        if (direction === 'right') {
+            //console.log('right', arr)
+            for (let col = 0 + colDeduct; col < n - colDeduct - 1; col++) {
+
+                count += 1;
+                arr[rowDeduct][col] = count;
+            };
+            return matrix(n, arr, subArr, 'down', colDeduct, rowDeduct, count)
+        }
+        if (direction === 'down') {
+            //console.log('down', arr)
+            for (let row = 0 + rowDeduct; row <= n - rowDeduct - 1; row++) {
+
+                count += 1;
+                arr[row][n - rowDeduct - 1] = count;
+            }
+            return matrix(n, arr, subArr, 'left', colDeduct, rowDeduct, count)
+        }
+        if (direction === 'left') {
+            //console.log('left', arr)
+            for (let col = n - colDeduct - 2; col >= colDeduct; col--) {
+
+                count += 1;
+                arr[n - rowDeduct - 1][col] = count;
+            };
+            return matrix(n, arr, subArr, 'up', colDeduct + 1, rowDeduct, count)
+        }
+        if (direction === 'up') {
+            //console.log('up', arr)
+            for (let row = n - rowDeduct - 2; row > rowDeduct; row--) {
+
+                count += 1;
+                arr[row][rowDeduct] = count;
+            }
+            return matrix(n, arr, subArr, 'right', colDeduct, rowDeduct + 1, count)
+        }
+    }
+    arr = [];
+    subArr = [];
 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
@@ -25,54 +66,8 @@ function matrix(n) {
         arr.push(subArr);
         subArr = [];
     }
-
-    function slide(arr, direction, colDeduct, rowDeduct, n, count) {
-        console.log(arr)
-        if (count === n * n) {
-            return arr;
-        } else {
-            switch (direction) {
-                case 'right':
-                    for (let col = 0 + colDeduct; col < n - colDeduct -1; col++) {
-                        count += 1;
-                        arr[rowDeduct][col] = count;
-                    };
-                    direction = 'down';
-                    slide(arr, direction, colDeduct, rowDeduct, n, count)
-                    break;
-
-                case 'down':
-                    for (let row = 0 + rowDeduct; row <= n - rowDeduct - 1; row++) {
-                        count += 1;
-                        arr[row][n - rowDeduct] = count;
-                    }
-                    direction = 'left';
-                    slide(arr, direction, colDeduct, rowDeduct, n, count)
-                    break;
-
-                case 'left':
-                    colDeduct += 1;
-                    for (let col = n - colDeduct - 1; col >= colDeduct; col--) {
-                        count += 1;
-                        arr[rowDeduct][col] = count;
-                    };
-                    direction = 'up';
-                    slide(arr, direction, colDeduct, rowDeduct, n, count)
-                    break;
-
-                case 'up':
-                    rowDeduct += 1;
-                    for (let row = n - rowDeduct - 1; row > rowDeduct; row--) {
-                        count += 1;
-                        arr[row][rowDeduct] = count;
-                    }
-                    direction = 'right';
-                    slide(arr, direction, colDeduct, rowDeduct, n, count)
-                    break;
-            }
-        }
-    }
-    return slide(arr, direction = 'right', colDeduct = 0, rowDeduct = 0, n, count = 0)
+    //console.log(arr)
+    return matrix(n, arr, subArr, 'right', colDeduct = 0, rowDeduct = 0, count = 0)
 
 }
 
